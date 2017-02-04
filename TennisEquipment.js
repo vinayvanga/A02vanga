b72e39bf729cf4c6f9f773f382fe30c791cc1823
vinayvanga <S528769@mail.nwmissouri.edu>
2017-02-03 22:34:26 -0600

    Add files via upload

---
+++ TennisEquipment.js
@@ -0,0 +1,12 @@
+function calculate(){
+
+    var A=document.getElementById("input1").value;
+     var B=document.getElementById("input2").value;
+
+     var result=parseInt(A)*parseInt(B)*3;
+     product.value=result;
+     event.preventDefault();
+
+
+
+}
